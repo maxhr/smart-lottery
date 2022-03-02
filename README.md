1968ee959bba85d618c29efed36985b1c45410a9
Max Harpunsky <max@dynamicyield.com>
2022-03-03 00:22:06 +0200

    fix near

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is a mono-repo for a smart contract on multiple blockchains. It aims to compare a (kind of) minumum viable code, for an actually useful smart-contract, on different blockchains.
 
 The contracts implement a lottery logic with these features:
-- A user can deposit tokens into the contract (`play_lottery` function)
+- A user can deposit tokens into the contract (`deposit` function)
 - A random number can be generated on chain (`get_random_seed` function)
 - The `trigger_lottery` function is triggered by external events
     - A real-world-time interval
@@ -13,7 +13,7 @@ The contracts implement a lottery logic with these features:
     - An multi-chain lottery - tokens across multiple chains are shared between winners on those chains, with price conversions and cross-chain value transfer
 
 
-| Chain     | `play_lottery` | `get_random_seed` | `trigger_lottery` | `share_prize` |
+| Chain     | `deposit`      | `get_random_seed` | `trigger_lottery` | `share_prize` |
 |-----------|----------------|-------------------|--------------|---------------|
 | Ethereum  |                |                   |              |               |
 | Cosmos    |                |                   |              |               |
--- near-smart-lottery/src/lib.rs
+++ near-smart-lottery/src/lib.rs
@@ -1,73 +1,68 @@
-use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, near_bindgen, AccountId, Balance};
+use near_sdk::{
+    env, near_bindgen, AccountId, Balance,
+    collections::{ UnorderedMap },
+};
 
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct SmartLottery {
-    participants: HashMap<AccountId, Balance>,
+    participants: UnorderedMap<AccountId, Balance>,
     prize_pool: Balance,
 }
 
 #[near_bindgen]
 impl SmartLottery {
 
+    #[init]
+    pub fn new(owner_id: AccountId) -> Self {
+        assert!(env::is_valid_account_id(owner_id.as_bytes()), "Invalid owner account");
+        assert!(!env::state_exists(), "Already initialized");
+        Self {
+            prize_pool: 0,
+            participants: UnorderedMap::new(b"credits".to_vec()),
+        }
+    }
+
     #[payable]
-    pub fn play(&mut self) {
-        let amount = env::attached_deposit();
-        let existing_amount = self.get_existing_amount_for_account(env::signer_account_id());
-        self.participants.insert(env::signer_account_id(), amount + existing_amount);
-        self.prize_pool += amount;
+    pub fn deposit(&mut self) {
+        let account_id = env::signer_account_id();
+        let deposit = env::attached_deposit();
+        let mut credits = self.participants.get(&account_id).unwrap_or(0);
+        credits = credits + deposit;
+        self.participants.insert(&account_id, &credits);
+        // env::log_str(&format!("==== {:?}", self.participants));
+        self.prize_pool += deposit;
     }
 
-    fn get_existing_amount_for_account(&self, account_id: AccountId) -> Balance {
-        let default: Balance = 0;
-        let existing_amount = self.participants.get(&account_id).unwrap_or(&default);
-        *existing_amount
+    pub fn get_random_seed(&mut self) -> u8 {
+        let rand: u8 = *env::random_seed().get(0).unwrap();
+        rand
     }
 
-    pub fn get_prize(&self) -> Balance {
+    pub fn get_grand_prize(&self) -> Balance {
         self.prize_pool
     }
 }
 
-/*
- * the rest of this file sets up unit tests
- * to run these, the command will be:
- * cargo test --package rust-template -- --nocapture
- * Note: 'rust-template' comes from Cargo.toml's 'name' key
- */
-
-// use the attribute below for unit tests
 #[cfg(test)]
 mod tests {
     use super::*;
     use near_sdk::test_utils::{accounts};
+    use near_sdk::test_utils::VMContextBuilder;
     use near_sdk::{testing_env, AccountId, Balance, VMContext};
  
-    // part of writing unit tests is setting up a mock context
-    // provide a `predecessor` here, it'll modify the default context
     fn get_context(signer_account_id: AccountId, attached_deposit: Balance) -> VMContext {
-        // let mut builder = VMContextBuilder::new();
-        VMContext {
-            current_account_id: signer_account_id.to_string(),
-            signer_account_id: signer_account_id.to_string(),
-            signer_account_pk: vec![0, 1, 2],
-            predecessor_account_id: accounts(1).to_string(),
-            input: Vec::new(),
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage: 0,
-            attached_deposit,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![0, 1, 2],
-            is_view: false,
-            output_data_receivers: vec![],
-            epoch_height: 19,
-        }
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(signer_account_id.clone())
+            .signer_account_id(signer_account_id.clone())
+            .attached_deposit(attached_deposit.clone())
+            .predecessor_account_id(accounts(1).clone())
+            .account_balance(0)
+            .build();
+        builder.context
     }
 
 
@@ -76,30 +71,26 @@ mod tests {
         let context1 = get_context(accounts(1), 115);
         testing_env!(context1);
         let mut contract = SmartLottery {
-            participants: HashMap::new(),
+            participants: UnorderedMap::new(b"credits".to_vec()),
             prize_pool: 0,
         };
-        contract.play();
-        // env::log_str(&contract.get_prize().to_string());
-        assert_eq!(115, contract.get_prize().clone());
-        assert_eq!(contract.participants.contains_key(&accounts(1)), true);
-        assert_eq!(*contract.participants.get(&accounts(1)).unwrap(), 115u128);
+        contract.deposit();
+        assert_eq!(115, contract.get_grand_prize().clone());
+        assert_eq!(contract.participants.get(&accounts(1)), Some(115u128));
 
         let context2 = get_context(accounts(2), 23);
         testing_env!(context2);
-        contract.play();
-        assert_eq!(115 + 23, contract.get_prize().clone());
-        assert_eq!(contract.participants.contains_key(&accounts(2)), true);
-        assert_eq!(*contract.participants.get(&accounts(2)).unwrap(), 23u128);
+        contract.deposit();
+        assert_eq!(115 + 23, contract.get_grand_prize().clone());
+        assert_eq!(contract.participants.get(&accounts(2)), Some(23u128));
 
         let context2a = get_context(accounts(2), 2);
         testing_env!(context2a);
-        contract.play();
-        assert_eq!(115 + 23 + 2, contract.get_prize().clone());
-        assert_eq!(*contract.participants.get(&accounts(2)).unwrap(), 23u128 + 2u128);
+        contract.deposit();
+        assert_eq!(115 + 23 + 2, contract.get_grand_prize().clone());
+        assert_eq!(contract.participants.get(&accounts(2)), Some(23u128 + 2u128));
 
         assert_eq!(contract.participants.len(), 2);
     }
 
 }
-// near deploy --wasmFile target/wasm32-unknown-unknown/release/smart_lottery.wasm --accountId mx2122.testnet
