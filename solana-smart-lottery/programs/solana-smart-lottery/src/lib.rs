4c14bdcbdc42ccd838a43d08141b0c292a9d4b1c
Max Harpunsky <max@dynamicyield.com>
2022-03-03 15:04:15 +0200

    solana first try

--- solana-smart-lottery/programs/solana-smart-lottery/src/lib.rs
+++ solana-smart-lottery/programs/solana-smart-lottery/src/lib.rs
@@ -1,14 +1,46 @@
 use anchor_lang::prelude::*;
+use std::collections::{HashMap};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod solana_smart_lottery {
     use super::*;
+
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let smart_lottery = &mut ctx.accounts.smart_lottery;
+        smart_lottery.authority = ctx.accounts.authtority.key();
+        smart_lottery.prize_pool = 0;
+        smart_lottery.participants = HashMap::new();
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        let smart_lottery = &mut ctx.accounts.smart_lottery;
+        smart_lottery.prize_pool += amount;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = smart_lottery, space = 48)]
+    pub smart_lottery: Account<'info, SmartLottery>,
+    pub authtority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut, has_one = authority)]
+    pub smart_lottery: Account<'info, SmartLottery>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct SmartLottery {
+    pub authority: Pubkey,
+    pub participants: HashMap<Pubkey, u64>,
+    pub prize_pool: u64,
+}
--- solana-smart-lottery/tests/solana-smart-lottery.ts
+++ solana-smart-lottery/tests/solana-smart-lottery.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { Keypair, SystemProgram } from "@solana/web3.js";
 import { SolanaSmartLottery } from "../target/types/solana_smart_lottery";
 
 describe("solana-smart-lottery", () => {
@@ -7,10 +8,18 @@ describe("solana-smart-lottery", () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.SolanaSmartLottery as Program<SolanaSmartLottery>;
+  const smartLottery = Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.rpc.initialize({});
+    const tx = await program.rpc.initialize({
+      accounts: {
+        smartLottery: smartLottery.publicKey,
+        authtority: program.provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [smartLottery],
+    });
     console.log("Your transaction signature", tx);
   });
 });
